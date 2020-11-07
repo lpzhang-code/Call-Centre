cca52a6bc841aec94afc34d4f8dc66b9baa682d7
Peter Zhang <lpzhang@me.com>
2020-11-07 17:18:44 +1100

    Update answer_sms.py

--- answer_sms.py
+++ answer_sms.py
@@ -4,11 +4,13 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 
-@app.route("/answer", methods=["GET", "POST"])
+@app.route("/message", methods=["GET", "POST"])
 def answer_call():
     """Respond to incoming SMS with our own SMS."""
     response = MessagingResponse()
-    response.message("Sent this SMS.")
+    response.message(
+        "Thank you for sending us an SMS. Please call us on this number or direct written enquiries to contact@anikalegal.com"
+    )
 
     return str(response)
 
